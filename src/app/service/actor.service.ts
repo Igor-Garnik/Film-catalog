b0170dc1ec0cd888bbae48f937b5ae4018ddbab9
Igor-Garnik <garnik_igor@ukr.net>
2018-07-24 12:34:47 +0300

    fix

--- src/app/service/actor.service.ts
+++ src/app/service/actor.service.ts
@@ -15,8 +15,7 @@ export class ActorService {
   constructor(private http: HttpClient, @Inject(API_CONFIG) public apiConfig: Config) { }
 
   getPopularActors(page: number = this.page) {
-<<<<<<< HEAD
-    return this.http.get(`${this.apiConfig.personUrl}/popular?${this.apiConfig.params}page=${page}`)
+    return this.http.get(`${this.dataService.personUrl}/popular?${this.dataService.params}page=${page}`)
       .pipe(map(data => {
         let actors = data['results'];
         this.page += 1;
@@ -28,19 +27,5 @@ export class ActorService {
           }
         })
       }))
-=======
-    return this.http.get(`${this.dataService.personUrl}/popular?${this.dataService.params}page=${page}`)
-    .pipe(map(data => {
-      let actors = data['results'];
-      this.page += 1;
-      return actors.map(actor => {
-        return {
-          title: actor.name,
-          voteAverage: actor.popularity,
-          posterPath: actor.profile_path
-        }
-      })
-    }))
->>>>>>> d6714bd911b996e21fc7df85a7a442e4ae31a362
   }
 }
