df46de4d5a26ae9658f8ca1ec5944c2a036b78b5
Igor-Garnik <garnik_igor@ukr.net>
2018-07-24 12:43:28 +0300

    test

--- src/app/film-catalog/search/search.component.ts
+++ src/app/film-catalog/search/search.component.ts
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
-=======
-import { Component, OnInit, Input, Output, EventEmitter  } from '@angular/core';
-import { MessageService } from  './../../service/message.service'
->>>>>>> d6714bd911b996e21fc7df85a7a442e4ae31a362
-=======
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-
->>>>>>> dev
 
 @Component({
   selector: 'app-search',
@@ -17,42 +7,15 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-<<<<<<< HEAD
-<<<<<<< HEAD
   query: string = '';
   @Input() item;
   @Output() update = new EventEmitter();
   constructor() { }
-=======
-  query:string = '';
-  @Input() item;
-  @Output() update = new EventEmitter();
-  constructor(public messageService : MessageService) { }
->>>>>>> d6714bd911b996e21fc7df85a7a442e4ae31a362
-=======
-  query: string = '';
-  @Input() item;
-  @Output() update = new EventEmitter();
-  constructor() { }
->>>>>>> dev
 
   sendQuery(query) {
     this.update.emit(this.query)
   }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-/*   sendQuery(query) {
-    this.messageService.sendMessage(query);
-  } */
-
-  clearMessage(): void {
-    this.messageService.clearMessage();
-  }
->>>>>>> d6714bd911b996e21fc7df85a7a442e4ae31a362
-=======
->>>>>>> dev
   ngOnInit() {
   }
 
